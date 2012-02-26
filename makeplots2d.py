965ed121c89486fd9db885997bcda4a3ab7f11e8
drreynolds <unknown>
2012-02-26 09:20:08 -0600

    Updated plotting scripts to no longer depend on prec.inp (since that is not part of this version of the code); removed prec.inp input file as well.

--- makeplots2d.py
+++ makeplots2d.py
@@ -11,7 +11,6 @@ times, energy, mass = mhd2d.load_cons()
 kenergy, menergy, ienergy = mhd2d.load_energies()
 xl, xr, yl, yr, zl, zr, ndump = mhd2d.load_mhd()
 mu, eta, kappa, gamma = mhd2d.load_props()
-#hprec, vprec, combo, nwaves, order, ADIcorr = mhd2d.load_prec()
 nx, ny, nz, xprocs, yprocs, zprocs, xbc, ybc, zbc = mhd2d.load_mesh()
 Newton, Krylov, Fnorm, dB, SolTime = mhd2d.load_RunHistory()
 
